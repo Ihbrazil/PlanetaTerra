c4a4bed86ecc2c67dde9c6e8ecab93bdb2e9f98b
ihbrazil <ihbrazil@users.noreply.github.com>
2024-11-29 10:08:03 -0300

    Atualização da 4ª Missão da Carreira Programador Mobile - Containers
    4ª Missão da Carreira Programador Mobile - Containers - Componente ImageBackground - imagem de fundo com as propriedades: style, source, blurRadius (desfoca a imagem) e resizeMode com os valores ( "cover" é o padrão, "contain", "stretch", "repeat", "center")

---
+++ estilos.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: '#0b4368',
+  },
+
+  box: {
+    backgroundColor: '#fff',
+    width: 300,
+    padding: 10
+  },
+
+  boxTitulo: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 15
+  },
+
+  boxParagrafo:{
+    fontSize: 16,
+    fontWeight: "normal",
+    marginBottom: 10
+  }
+});
+
+export default estilos;
